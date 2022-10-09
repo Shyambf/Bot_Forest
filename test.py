6283fa446e8d16b4737c31ec0500274ccf5e026f
shyam13472 <samborisov751@gmail.com>
2022-10-09 17:16:44 +0300

    add test and docker

---
+++ test.py
@@ -0,0 +1,4 @@
+from src.main import pattern_photo
+# загружаешь в папку фаил old.jpg или меняешь первый параметр
+pattern_photo('old.jpg', 'new.jpg', False, True, 'test', 'test', '13 ktn', '234', 'asdfg', 'feggeg', 'wrgwrg', 'oiuytfrdsz')
+pattern_photo('old.jpg', 'new2.jpg', True, True, 'test', 'test', '13 ktn', '234', 'asdfg', 'feggeg', 'wrgwrg', 'oiuytfrdsz')
